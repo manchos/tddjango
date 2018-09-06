5b16ba544d02fff177b86b26ef0fedc06d0e65f5
manchos <vladm.ru@gmail.com>
2018-09-07 01:37:44 +0300

    ferst modul test and resolving url

---
+++ lists/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+from django.urls import resolve
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
