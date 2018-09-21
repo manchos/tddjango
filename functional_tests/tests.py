c8de1b5bc121525b8f887a476abfe87cfced6541
manchos <vladm.ru@gmail.com>
2018-09-21 22:48:53 +0300

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,11 +20,10 @@ class NewVisitorTest(LiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
-
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_element_by_tag_name('tr')
+                rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
             except (AssertionError, WebDriverException) as e:
