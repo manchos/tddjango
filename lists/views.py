8fccbcadd5ddc01577b4261191a7e99c9747abfe
manchos <vladm.ru@gmail.com>
2018-09-26 00:14:40 +0300

    Новый URl + представление для добавления в существующие списки. ФТ проходит

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/единственный-список/')
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+    else:
+        Item.objects.create(text='A new list item', list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
